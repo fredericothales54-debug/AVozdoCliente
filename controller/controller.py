97dd64c0338a0a254cb3a6c09c533ba482064f6c
Tiago rodrigues de Souza <tiagorodriguessk196@gmail.com>
2025-11-27 21:38:19 -0300

    incrementçao do codigo com as funçoes de autentica

--- controller/controller.py
+++ controller/controller.py
@@ -1,6 +1,7 @@
 import psycopg2
 import model
 import view
+import datetime
 from model import clientedb
 from view import AppView
 class appcomtroll:
@@ -37,3 +38,33 @@ class appcomtroll:
             self.view.mostar_mensagem(f" item '{nome}' cadastrado com sucesso!")
         except Exception as e:
             self.view.mostar_menssagem(f" erro cadastrar produto ")
+    def realizar_emprestimo(self):
+        """Lógica para registrar um empréstimo."""
+        # Implementação futura: 
+        # 1. Obter ID do item e ID do usuário via View.
+        # 2. Chamar o Model para criar um registro na tabela Movimentacao.
+        self.view.mostrar_mensagem("Função de Empréstimo a ser implementada...")
+    
+    def finalizar_app(self):
+        """Fecha a conexão e encerra o aplicativo."""
+        self.running = False
+        if self.db_conn:
+            self.db_conn.close()
+        self.view.mostrar_mensagem("Aplicação encerrada. Conexão com o DB fechada.")
+
+class inventarioContrller:
+    def __init__(self,db_conn):
+        if db_conn is None:
+            raise ValueError("conexao com o banco de dados nao pode ser nula.")
+        self.db_conn = db_conn
+        self.db_model = conexaobanco_model(self.db_conn)
+    def autenticar_usuario(self,matricula:str):
+        query = """
+            SELECT id_usuario, nome, matricula, senha
+            FROM usuario
+            WHERE matricula = %s;
+        """
+        row = self._executar_query(query, (matricula,), fetchone=True)
+        if row:
+            return (row)
+        return None     
